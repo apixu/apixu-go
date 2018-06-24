4cdaba2470996bb7d8dfdfaa76e40bc931ee4161
Andrei Avram <avramandrei@ymail.com>
2018-06-24 04:13:50 +0300

    Fix XML error message from API.

--- response/error.go
+++ response/error.go
@@ -1,13 +1,26 @@
 package response
 
-// Error offers extra error info
+import (
+	"encoding/xml"
+)
+
+// Error offers error info received from the API
 type Error struct {
-	Error ErrorResponse `json:"error" xml:"error"`
+	Error ErrorResponse `json:"error" xml:">error"`
 }
 
-// ErrorResponse provides error code and message
-// for errors to be handled specifically
+// ErrorResponse provides error code and message for errors to be handled specifically
 type ErrorResponse struct {
 	Code    uint16 `json:"code" xml:"code"`
 	Message string `json:"message" xml:"message"`
+}
+
+// UnmarshalXML inserts the API error response into the error element
+func (e *Error) UnmarshalXML(d *xml.Decoder, start xml.StartElement) (err error) {
+	var el *ErrorResponse
+	if err := d.DecodeElement(&el, &start); err != nil {
+		return err
+	}
+	e.Error = *el
+	return nil
 }
