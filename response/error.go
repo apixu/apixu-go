af348a30fc86e994919137c92febef5e28e48f3c
Andrei Avram <avramandrei@ymail.com>
2018-06-26 20:58:33 +0300

    Remove unnecessary xml tag.

--- response/error.go
+++ response/error.go
@@ -4,7 +4,7 @@ import "encoding/xml"
 
 // Error offers error info received from the API
 type Error struct {
-	Error ErrorResponse `json:"error" xml:">error"`
+	Error ErrorResponse `json:"error"`
 }
 
 // ErrorResponse provides error code and message for errors to be handled specifically
